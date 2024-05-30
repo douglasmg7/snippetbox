9f221b08be352a46a70be7539457125641ed4a89
douglasmg7 <douglasmg7@gmail.com>
2024-05-30 19:51:09 -0300

    main and handlers

---
+++ cmd/web/handlers.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"html/template" // New import
+	"log"           // New import
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "Go")
+
+	// Include the navigation partial in the template files.
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
+// Add a snippetView handler function.
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+// Add a snippetCreate handler function.
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+}
+
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	// Use the w.WriteHeader() method to send a 201 status code.
+	w.WriteHeader(201)
+
+	// Then w.Write() method to write the response body as normal.
+	w.Write([]byte("Save a new snippet..."))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/douglasmg7/snippetbox
+
+go 1.22.2
