a1e86821d9622936d44aad51dd660ae58ee7dc8e
douglasmg7 <douglasmg7@gmail.com>
2024-09-11 11:35:54 -0300

    Comment on / route using {$}.

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,7 +9,7 @@ func (app *application) routes() *http.ServeMux {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /{$}", app.home)	// {$} Restrict this route to exact matches on / only.
 	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
